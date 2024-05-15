b7db3da7915c93366217e36ca0c906633f196fbf
Nicolas de Lima <nicolasdaniel2106@gmail.com>
2024-05-15 18:18:54 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// function muda_cor() {
+//     var butao = document.getElementById("botao")
+//     butao.style.backgroundColor = "red"
+// }
+
+// function retorna_cor() {
+//     var butao = document.getElementById("botao")
+//     butao.style.backgroundColor = "yellow"
+// }
+
+function mostra_elementos() {
+    var classes = document.getElementsByClassName("oculto")
+
+    for (i = 0; i < classes.length; i++) {
+        classes[i].style.display = "block"
+    }
+}
